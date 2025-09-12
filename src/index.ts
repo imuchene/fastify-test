bb6eb0905021404b758781827b4066d00e837639
Isaiah Muchene <imuchene@msn.com>
2025-09-12 16:57:06 +0300

    Moved all source files to the source directory

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import fastifyAutoload from '@fastify/autoload';
+import fastify from 'fastify';
+import path from 'path';
+import { routes } from './routes/our-first-route';
+
+const app = fastify();
+
+// server.register(fastifyAutoload, {
+//   dir: path.join(__dirname, 'plugins'),
+// });
+
+app.listen({ port: 8080}, (error, address) => {
+  if (error) {
+    console.error(error);
+    process.exit(1)
+  }
+
+  console.log(`Server listening at ${address}`)
+})
+
+app.register(fastifyAutoload, {
+  dir: path.join(__dirname, 'routes'),
+})
+
+// app.register(testRoute);
+
+app.register(routes)
+
+
+app.get('/ping', async () => {
+  return 'pong \n';
+});
