3f546e5dd377143ac30d791eeddd37ebf6c6547a
Isaiah Muchene <imuchene@msn.com>
2025-09-25 18:44:42 +0300

    Added functionality for inputting custom recipes to the feed

--- src/modules/content-aggregation-feed/content-aggregation-feed.ts
+++ src/modules/content-aggregation-feed/content-aggregation-feed.ts
@@ -1,17 +1,22 @@
 import Parser from 'rss-parser';
+import promptModule from 'prompt-sync';
 
 const parser = new Parser();
+
 const urls = [
   'https://www.bonappetit.com/feed/recipes-rss-feed/rss',
   'https://www.budgetbytes.com/category/recipes/feed/',
   'https://www.reddit.com/r/recipes/.rss',
 ];
 
+const prompt = promptModule({ sigint: true });
+const customItems: any = [];
+
 async function main() {
   const feedItems: any[] = [];
   const awaitableRequests = urls.map((url) => parser.parseURL(url));
-
   const responses = await Promise.all(awaitableRequests);
+
   aggregate(responses, feedItems);
   print(feedItems);
 }
@@ -27,12 +32,14 @@ function aggregate(responses: any, feedItems: any[]) {
   return feedItems;
 }
 
-function print(feedItems: any) {
+function print(feedItems: any[]) {
+  const res = prompt('Add item: ');
+  const [title, link] = res.split(',');
+  if (![title, link].includes(undefined)) customItems.push({ title, link });
+
   console.clear();
-  console.table(feedItems);
+  console.table(feedItems.concat(customItems));
   console.log('Last Updated', new Date().toUTCString());
 }
-
-// setInterval(main, 2000);
 
 main();
