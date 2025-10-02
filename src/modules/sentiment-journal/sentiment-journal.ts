7070edb25991a47a3097761a02fafbef7b897260
Isaiah Muchene <imuchene@msn.com>
2025-10-02 16:56:39 +0300

    - Removed the stopwords from the input string - Found the stems of the input string

--- src/modules/sentiment-journal/sentiment-journal.ts
+++ src/modules/sentiment-journal/sentiment-journal.ts
@@ -1,15 +1,18 @@
 import SpellChecker from 'spellchecker';
+import natural from 'natural';
+import { removeStopwords } from 'stopword'
 
-const inputString: string = 'I am feling grat!';
+const inputString: string = 'I am feeelinng grea!';
+const tokenizer = new natural.WordTokenizer()
 
-const correcSpelling = (inputString: string ) => {
+function correctSpelling(inputString: string ){
   const words = inputString.split(' ');
   const corrections = [];
 
   for (const word of words) {
     if (SpellChecker.isMisspelled(word)) {
       const options = SpellChecker.getCorrectionsForMisspelling(word);
-      corrections.push(options[1])
+      corrections.push(options[0])
     }
     else {
       corrections.push(word);
@@ -19,5 +22,26 @@ const correcSpelling = (inputString: string ) => {
   return corrections.join(' ');
 }
 
-console.log(correcSpelling(inputString));
+function tokenizeInput(inputString: string){
+  return tokenizer.tokenize(inputString);
+}
+
+function stemWords(tokens:Array<any>) {
+  const stems = [];
+  for (const token of tokens) {
+    const stem = natural.PorterStemmer.stem(token);
+    stems.push(stem);
+  }
+
+  return stems;
+}
+
+const correctedSpelling = (correctSpelling(inputString));
+const tokens = tokenizeInput(correctedSpelling);
+const stems = stemWords(tokens);
+const removedStopwords = removeStopwords(stems);
+
+console.log('tokens', tokens);
+console.log('stems', stems);
+console.log('removedStopwords', removedStopwords);
 
