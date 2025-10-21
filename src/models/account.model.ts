536bceaebef2708ec14ca485ffb46de1c203b6b9
Isaiah Muchene <imuchene@msn.com>
2025-10-21 18:34:22 +0300

    Created the registration and authentication logic in the account model

---
+++ src/models/account.model.ts
@@ -0,0 +1,118 @@
+import { DataTypes, Model } from 'sequelize';
+import { db } from '../db/database.config';
+import crypto from 'node:crypto';
+
+export class Account extends Model {
+  declare username: string;
+  declare hash: string;
+  declare confirmPassword: string;
+  declare salt: string;
+
+  static async findByUsername(username: string): Promise<Account | null> {
+    const lowerCaseUsername = username.toLowerCase();
+    return await this.findOne({ where: { username: lowerCaseUsername } });
+  }
+
+  static async register(username: string, password: string): Promise<Account> {
+    const existingAccount = await this.findByUsername(username);
+    if (existingAccount) {
+      throw new Error('Account already exists');
+    }
+    const account = this.build({ username });
+    account.setPassword(password);
+    return await account.save();
+  }
+
+  setPassword(password: string): void {
+    if (!password) {
+      throw new Error('No password supplied');
+    }
+
+    try {
+      const bufferBytes = crypto.randomBytes(32);
+      const salt = bufferBytes.toString('hex');
+      const hashRaw = crypto.pbkdf2Sync(password, salt, 12000, 64, 'sha512');
+      this.set('hash', Buffer.from(hashRaw).toString('hex'));
+      this.set('salt', salt);
+    } catch (error) {
+      if (error && error instanceof Error) {
+        throw new Error(error.message);
+      } else {
+        throw new Error('Unable to hash password');
+      }
+    }
+  }
+
+  authenticate(password: string): boolean {
+    const { salt, hash } = this;
+    if (!salt) {
+      throw new Error('No salt found');
+    }
+
+    const hashRaw = crypto.pbkdf2Sync(password, salt, 12000, 64, 'sha512');
+    const currentHash = Buffer.from(hashRaw).toString('hex');
+    return currentHash === hash;
+  }
+
+  static passportAuthenticate(): CallableFunction {
+    return async (
+      username: string,
+      password: string,
+      done: CallableFunction,
+    ) => {
+      try {
+        const account = await this.findByUsername(username);
+
+        if (!account) {
+          return done(null, false, { message: 'User not found ' });
+        }
+
+        const isValid = account.authenticate(password);
+        if (isValid) {
+          return done(null, account);
+        } else {
+          return done(null, false, { message: 'Password incorrect' });
+        }
+      } catch (error) {
+        return done(error);
+      }
+    };
+  }
+}
+
+Account.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'Account',
+    tableName: 'accounts',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+);
+
+Account.beforeCreate((account) => {
+  account.username = account.username.toLowerCase();
+});
+
+Account.sync({ alter: true });
--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,8 +1,11 @@
-import { FastifyInstance, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import {
   AuthQueryString,
   LoginFormVariables,
 } from '../interfaces/auth-variables.interface';
+import { Account } from '../models/account.model';
+
+const users: any = {};
 
 export async function authRoutes(fastify: FastifyInstance) {
   const loginFormVars: LoginFormVariables = {
@@ -31,6 +34,30 @@ export async function authRoutes(fastify: FastifyInstance) {
       const { page } = request.query;
       const formVars = loginFormVars[page] || loginFormVars.signup;
       return reply.hbsView('/views/authentication/index.hbs', formVars);
+    },
+  );
+
+  fastify.post(
+    '/auth/signup',
+    async (request: FastifyRequest<{ Body: Account }>, reply: FastifyReply) => {
+      const { username, hash, confirmPassword } = request.body;
+      if (hash != confirmPassword) {
+        return reply.send({ message: 'Passwords do not match' });
+      }
+
+      await Account.create({ username, hash });
+      return reply.send({ message: 'Account created' });
+    },
+  );
+
+  fastify.post(
+    '/auth/login',
+    async (request: FastifyRequest<{ Body: Account }>, reply: FastifyReply) => {
+      const { username, hash } = request.body;
+      if (users[username] && users[username] == hash) {
+        return reply.send({ message: 'Logged in' });
+      }
+      return reply.redirect('/auth?page=login');
     },
   );
 }
