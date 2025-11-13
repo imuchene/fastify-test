d57667f19caa08091d484f5ffb1515fdee97fee7
Isaiah Muchene <imuchene@msn.com>
2025-11-13 20:03:38 +0300

    Added some context injection to the AI prompt

--- src/modules/interview-atlas-ai/index.ts
+++ src/modules/interview-atlas-ai/index.ts
@@ -3,13 +3,27 @@ import '@dotenvx/dotenvx/config';
 
 const ai = new GoogleGenAI({});
 
-async function main() {
+async function main(prompt: string) {
   const response = await ai.models.generateContent({
-    model: 'gemini-2.0-flash',
-    contents: 'In one sentence, explain Node',
+    model: 'gemini-2.5-flash-lite',
+    contents: [
+      {
+        role: 'user',
+        parts: [
+          {
+            text: `You are an AI assistant that helps users learn programming and prepare for
+        technical interviews. Provide clear explanations with examples when needed.`,
+          },
+        ],
+      },
+      {
+        role: 'user',
+        parts: [{ text: prompt }],
+      },
+    ],
   });
 
   console.log('AI response', response.text);
 }
 
-main();
+main('In one sentence, explain recursion');
